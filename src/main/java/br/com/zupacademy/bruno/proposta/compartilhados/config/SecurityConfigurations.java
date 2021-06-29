f80758bcd3cb1484e7401e09a4b9a18a947bee45
Bruno Valle <brunolv6@gmail.com>
2021-06-28 21:26:24 -0300

    (feat):(Security/Keyckoak) define security police and configure keycloak on API, add dependencies of security and OAuth2

---
+++ src/main/java/br/com/zupacademy/bruno/proposta/compartilhados/config/SecurityConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.bruno.proposta.compartilhados.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
--- src/main/java/br/com/zupacademy/bruno/proposta/detalhesProposta/DetalhesPropostaController.java
+++ src/main/java/br/com/zupacademy/bruno/proposta/detalhesProposta/DetalhesPropostaController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +28,10 @@ public class DetalhesPropostaController {
 	
 	@GetMapping("/{id}/detalhes")
 	@Transactional
-	public ResponseEntity<?> detalhar(@PathVariable Long id) {
-		
+	public ResponseEntity<?> detalhar(@PathVariable Long id, Authentication authentication) {
+
+		System.out.println(authentication.getName());
+
 		Proposta proposta = em.find(Proposta.class, id);
 		
 		if(proposta == null) {
